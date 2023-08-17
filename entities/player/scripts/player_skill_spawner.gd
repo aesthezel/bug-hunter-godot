edf22fa93de812dabc883d9d4515c1731d025070
David Pino <aesthezel@gmail.com>
2023-08-17 01:38:59 -0400

    Ya funciona parte del stack overflow

---
+++ entities/player/scripts/player_skill_spawner.gd
@@ -0,0 +1,34 @@
+class_name PlayerSkillSpawner extends Node2D
+
+@export var instance_offset : Vector2 = Vector2.ZERO
+@export var activator_ray : PlayerActivatorRay
+@export var skill_stack_overflow_packed : PackedScene
+@export var input_disabled_time : float = 1.0
+@export var input_timer : Timer
+
+var _can_input : bool = true
+
+func disable_input(time):
+	_can_input = false
+	input_timer.start(time)
+
+func enable_input():
+	_can_input = true
+
+func _ready():
+	input_timer.timeout.connect(enable_input)
+
+func _process(delta):
+	if Input.is_action_just_pressed("move_left"):
+		instance_offset.x = instance_offset.x * -1
+	if Input.is_action_just_pressed("move_right"):
+		instance_offset.x = abs(instance_offset.x)
+	
+	if _can_input:
+		if Input.is_action_just_pressed("attack") and !activator_ray.is_colliding():
+			disable_input(input_disabled_time)
+			activator_ray.sleep_ray(input_disabled_time)
+			
+			var skill = skill_stack_overflow_packed.instantiate() as SkillStackOverflow
+			owner.add_child(skill)
+			skill.global_position = global_position + instance_offset
--- entities/player/scripts/player_skill_stack_overflow.gd
+++ entities/player/scripts/player_skill_stack_overflow.gd
@@ -1,28 +1,27 @@
 class_name SkillStackOverflow extends RigidBody2D
 
-@export var forceVelocity : float = 300.0
-@export var forceTimer : Timer
-@export var skillTime : float = 10
+@export var force_velocity : float = 300.0
+@export var force_timer : Timer
+@export var skill_time : float = 10
 
-var isActivated : bool = false
+var _is_activated : bool = false
 
 func activate_skill(direction : float):
+	var custom_direction = -1 if direction < 0 else 1
+	force_velocity = force_velocity * custom_direction
 	
-	var customDirection = -1 if direction < 0 else 1
-	forceVelocity = forceVelocity * customDirection
-	
-	forceTimer.start(skillTime)
-	isActivated = true
+	force_timer.start(skill_time)
+	_is_activated = true
 
 func disable_skill():
-	isActivated = false
+	_is_activated = false
 	queue_free()
 
 func _ready():
-	forceTimer.timeout.connect(disable_skill)
+	force_timer.timeout.connect(disable_skill)
 
 func _physics_process(delta):
-	if isActivated:
-		set_axis_velocity(Vector2(forceVelocity, 0.0))
+	if _is_activated:
+		set_axis_velocity(Vector2(force_velocity, 0.0))
 	else:
 		linear_velocity = Vector2.ZERO
